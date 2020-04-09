68f86c60733b4e2a467ecea64fe310de41b2d501
prashanttomar23 <prashant.tomar@rapidops.com>
2020-04-09 19:08:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import Hello from './components/hello';
-import Welcome from './components/welcome';
-import Message from './components/message';
-import Counter from './components/Counter';
-import FunctionClick from './components/functionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
+// import Hello from './components/hello';
+// import Welcome from './components/welcome';
+// import Message from './components/message';
+// import Counter from './components/Counter';
+// import FunctionClick from './components/functionClick';
+// import ClassClick from './components/ClassClick';
+// import EventBind from './components/EventBind';
+// import ParentComponent from './components/ParentComponent';
+// import UserGreeting from './components/UserGreeting';
+// import NameList from './components/NameList';
 import Form from './components/Form';
 import Clock from './components/Clock'
 import ParentRef from './components/ParentRef';
-import Text from './components/Text';
+
 class App extends Component {
   render(){
     return (
@@ -36,7 +36,7 @@ class App extends Component {
         <Clock></Clock>
         <Form></Form>
         <ParentRef></ParentRef>
-        {/* <Text></Text> */}
+        
         {/* <NameList></NameList> */}
         
       </div>
